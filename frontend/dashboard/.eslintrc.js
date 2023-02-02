a487fdc802400f4ff7d7bfa2aaa526537cec946b
AdonaiSoaresM <adonaielite@gmail.com>
2023-02-02 10:07:32 -0300

    Home Contact component

--- frontend/dashboard/.eslintrc.js
+++ frontend/dashboard/.eslintrc.js
@@ -3,17 +3,16 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/vue3-essential', '@vue/standard'],
   parserOptions: {
     parser: '@babel/eslint-parser'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'vue/multi-word-component-names': 0
+    'vue/multi-word-component-names': 0,
+    'space-before-function-paren': ['error', 'never']
+    //  'vue/no-multiple-template-root': 'off'
   },
   overrides: [
     {
--- frontend/dashboard/src/views/Home/index.vue
+++ frontend/dashboard/src/views/Home/index.vue
@@ -1,14 +1,27 @@
 <template>
-  <h1 class="font-black text-brand-main">Aqui é a Home</h1>
-  <custom-header />
+  <custom-header @create-account="handleAccountCreate" @login="handleLogin" />
   <contact />
-  <div>
-    <p>FeedBacker 2021</p>
+  <div class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">FeedBacker © 2023</p>
   </div>
 </template>
 
 <script>
-export default {
+import CustomHeader from './CustomHeader.vue'
 
+import Contact from './Contact.vue'
+
+export default {
+  components: { CustomHeader, Contact },
+  methods: {
+    handleLogin() {},
+    handleAccountCreate() {}
+  },
+  mounted() {
+    const token = window.localStorage.getItem('token')
+    if (token) {
+      this.$router.push({ name: 'Feedbacks' })
+    }
+  }
 }
 </script>
